4aeffcac64db2049bb931651ca5c91bb375fd081
AI-JiangBBOC <40155137+AI-JiangBBOC@users.noreply.github.com>
2018-10-20 18:02:24 +0800

    Delete Exercise01_09.java

--- Exercise01_09.java
+++
@@ -1,9 +0,0 @@
-public class Exercise01_09 {
-  public static void main(String[] args) {
-  	// Display area
-    System.out.println(4.5 * 7.9);
-
-    // Display perimeter
-    System.out.println(2 * (4.5 + 7.9));
-  }
-}
